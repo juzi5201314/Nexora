91f2af62465ec8cc954f68c15f808c06d9dd6241
soeur <juzi201314@gmail.com>
2025-09-26 18:10:24 +0800

    [feat] 新的存储格式支持和一些平衡性调整

---
+++ Source/Nexora/comp/CompDataFormatType.cs
@@ -0,0 +1,17 @@
+﻿using Verse;
+
+namespace Nexora.comp;
+
+public class CompDataFormatType: CompDataFormat
+{
+    public override int GetCountCanAccept(ItemStorage storage, Thing item)
+    {
+        var thingCount = storage.Count;
+        return thingCount < Props.Value ? int.MaxValue : 0;
+    }
+
+    public override IEnumerable<string> GetExtraInspectString(ItemStorage storage)
+    {
+        yield return $"{"Capacity".Translate()}: {storage.Count} / {Props.Value} types";
+    }
+}
